b9e692e71713387dec0bff3ddb57fca6966db8f5
slackbot115 <viniccius.carvalho22@gmail.com>
2021-07-20 15:43:11 -0300

    nham

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import json
 import time
 import hashlib
 
+
 def hashFile(filename):
    hObj = hashlib.sha256()
 
@@ -16,11 +17,24 @@ def hashFile(filename):
 
    return hObj.hexdigest()
 
+
+def progressBar(filename, filesize):
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            s.sendall(bytes_read)
+            progress.update(len(bytes_read))
+    s.shutdown(socket.SHUT_WR)
+
+
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
 
-host = "192.168.1.6"  # deixar o usuario escolher AAAAAAAAAAAAAAAAAAAAAAAAA
-port = 5001
+host = "192.168.1.3"
+port = 5000
 
 s = socket.socket()
 s.connect((host, port))
@@ -66,24 +80,11 @@ while True:
         payload = json.dumps(payload)
         s.send(bytes(payload, 'utf-8'))
 
-        # start sending the file
-        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        with open(filename, "rb") as f:
-            while True:
-                # read the bytes from the file
-                bytes_read = f.read(BUFFER_SIZE)
-                if not bytes_read:
-                    # file transmitting is done
-                    break
-                # we use sendall to assure transimission in
-                # busy networks
-                s.sendall(bytes_read)
-                # update the progress bar
-                progress.update(len(bytes_read))
+        progressBar(filename, filesize)
 
         pacote = s.recv(BUFFER_SIZE)
         pacote_dumped = json.loads(pacote)
         if pacote_dumped["status"] == "success":
-            print("Arquivo enviado com sucesso")
+            print("\nArquivo enviado com sucesso")
         else:
-            print("Falha no envio do arquivo")
+            print("\nFalha no envio do arquivo")
